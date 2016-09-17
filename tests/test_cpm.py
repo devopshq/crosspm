f444d5714b758ec7c880c50a7cd15e26fafc3edd
Nikolay Yusev <nyusev@ptsecurity.com>
2016-09-17 17:34:12 +0700

    test for constructor

--- tests/test_cpm.py
+++ tests/test_cpm.py
@@ -1,14 +1,9 @@
 # -*- coding: utf-8 -*-
-import json
-import os
-import subprocess
 
-from docopt import docopt
-import logging
 import pytest
-import crosspm
+import sys
+import traceback
 from crosspm.cpm import App
-from crosspm.helpers.config import CROSSPM_DEPENDENCY_LOCK_FILENAME
 
 
 class TestCpm:
@@ -24,26 +19,78 @@ class TestCpm:
     def teardown(self):
         print("TestCpm - teardown method")
 
-    def test_add(self):
-        assert 2 + 2 == 4
+    # def test_check_common_args(self):
+    #     sys.argv.clear()
+    #     sys.argv.append(1)
+    #     sys.argv.insert(1,'--help')
+    #     argv = sys.argv[1:]
+    #     try:
+    #         app = App().__init__(argv)
+    #     except SystemExit as e:
+    #         DebugMsg("Execute command: {}".format(e))
+    #         print("App__init__")
+    #     assert App.check_common_args()
 
-    def test_mul(self):
-        assert 3 * 3 == 9
+    def test_argv_help(self):
+        sys.argv.clear()
+        sys.argv.append(1)
+        sys.argv.insert(1, '---help')
+        argv = sys.argv[1:]
+        try:
+            app = App().__init__(argv)
+        except SystemExit as e:
+            if e.code == None:
+                assert True
+            else:
+                assert False
 
-    def test_check_common_args(self):
-        test = {'--config': 'D:/crosspm-1/tests/crosspm.yaml','--depslock-path': './cpm.manifest','--help': False,'--options': 'contract=R11.0,quality=stable,os=linux','--out-format': 'stdout',
-         '--out-prefix': '',
-         '--output': None,
-         '--verbose': True,
-         '--verbosity': '30',
-         '--version': False,
-         '<OUT>': None,
-         '<SOURCE>': None,
-         'download': True,
-         'pack': False,
-         'promote': False}
+    #     sys.argv.clear()
+    #     sys.argv.append(1)
+    #     sys.argv.insert(1, '-help')
+    #     argv = sys.argv[1:]
+    #     try:
+    #         app = App().__init__(argv)
+    #     except SystemExit as e:
+    #         assert True
+    #
+    # def test_argv_h(self):
+    #     sys.argv.clear()
+    #     sys.argv.append(1)
+    #     sys.argv.insert(1, '-h')
+    #     argv = sys.argv[1:]
+    #     try:
+    #         app = App().__init__(argv)
+    #     except SystemExit as e:
+    #         assert True
+    #
+    #     sys.argv.clear()
+    #     sys.argv.append(1)
+    #     sys.argv.insert(1, '--h')
+    #     argv = sys.argv[1:]
+    #     try:
+    #         app = App().__init__(argv)
+    #     except SystemExit as e:
+    #         assert True
 
-        arguments = docopt(test, argv=test,version='0.8.1')
 
-        app = App(arguments)
-        assert App.check_common_args()
+
+
+
+# test = {'--config': 'D:/crosspm-1/tests/crosspm.yaml', '--depslock-path': './cpm.manifest', '--help': False,
+#         '--options': 'contract=R11.0,quality=stable,os=linux', '--out-format': 'stdout',
+#         '--out-prefix': '',
+#         '--output': None,
+#         '--verbose': True,
+#         '--verbosity': '30',
+#         '--version': False,
+#         '<OUT>': None,
+#         '<SOURCE>': None,
+#         'download': True,
+#         'pack': False,
+#         'promote': False}
+
+# def test_add(self):
+    #     assert 2 + 2 == 4
+    #
+    # def test_mul(self):
+    #     assert 3 * 3 == 9
