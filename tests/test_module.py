698cdc573c101e99d6a7f64680c67b5be681d210
Alexander Kovalev <ak@alkov.pro>
2016-12-15 20:00:40 +0300

    test_module prints packages with multiple versions found

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from crosspm import CrossPM
-# argv = 'download -c "Z:\\home\\positive\\crosspm\\test\\test6\\crosspm.yaml" ' \
-#        '--depslock-path="Z:\\home\\positive\\crosspm\\test\\test6\\dependencies.txt" ' \
-#        '--out-format=shell ' \
-#        '--output=found.sh'
-# argv = 'download -c "Z:\\home\\positive\\crosspm\\test\\test6\\crosspm.yaml" --depslock-path="Z:\\home\\positive\\crosspm\\test\\test6\\dependencies.txt"'
-argv = 'download -c "Z:\\home\\positive\\crosspm\\test\\test11\\config.yaml" --depslock-path="Z:\\home\\positive\\crosspm\\test\\test11\\dependencies.txt"'
-# argv = 'download --depslock-path="Z:\\home\\positive\\crosspm\\test\\test6\\dependencies.txt"'
-print(CrossPM(argv, return_result=True).run())
+import sys
+
+argv = 'download -c "Z:\\home\\positive\\crosspm\\test\\test14\\crosspm.yaml" --depslock-path="Z:\\home\\positive\\crosspm\\test\\test14\\dependencies.txt" --no-fails'
+err, res = CrossPM(argv, return_result=True).run()
+if err == 0:  # Если не возникло других ошибок
+    sys.stderr.write("\nCheck for multiple versions:\n\n")
+    versions = {}
+    for item in res:
+        ver = versions.get(item['package'], [])
+        ver_number = str(item['version'])
+        if ver_number not in ver:
+            ver.append(ver_number)
+        versions[item['package']] = ver
+    success = True
+    for item, ver in versions.items():
+        if len(ver) > 1:
+            sys.stderr.write("{}: {}\n".format(item, str(ver)))
+            success = False
+    if success:
+        sys.stderr.write("\nAll Ok!\n")
+else:
+    sys.stderr.write("\n{}\n{}".format(err, res))
