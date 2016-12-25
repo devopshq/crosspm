4e547e3d6ffea672958140b70f67650a94b1e2ee
Alexander Kovalev <ak@alkov.pro>
2016-12-25 05:54:15 +0300

    temp

--- tests/test_module_raw.py
+++ tests/test_module_raw.py
@@ -5,7 +5,7 @@ import sys
 from treelib import Tree
 
 
-def to_dict(packages, _tree=None, _id=None, _flat=None):
+def tree_dict(packages, _tree=None, _id=None, _flat=None):
     if _tree is None:
         _tree = Tree()
         _id = '<root>'
@@ -24,7 +24,7 @@ def to_dict(packages, _tree=None, _id=None, _flat=None):
             ver.append(pkg_ver)
         _flat[pkg_name] = ver
         if packages[_pkg].packages:
-            _res = to_dict(packages[_pkg].packages, _tree, pkg_id, _flat)
+            _res = tree_dict(packages[_pkg].packages, _tree, pkg_id, _flat)
             _flat = _res[1]
     return _tree, _flat
 
@@ -34,20 +34,19 @@ err, res = CrossPM(argv, return_result='raw').run()
 if err == 0:  # Если не возникло других ошибок
     sys.stderr.write("\nCheck for multiple versions:\n\n")
 
-    tree, flat = to_dict(res)
-    max_len = max(len(x) for x in str(tree).split('\n'))
+    tree, flat = tree_dict(res)
+    max_len = max(len(tree.nodes[x].tag) for x in tree.nodes)
 
     success = True
-    for item, ver in flat.items():
-        if len(ver) > 1:
-            for node in tree.nodes:
-                if node != '<root>':
-                    node_name = tree.nodes[node].tag
-                    spaces = ((tree.depth() - tree.depth(node)) * 4) + (max_len - len(node_name))
-                    tree.nodes[node].tag += '{} {}    {}'.format(' ' * spaces,
-                                                                 tree.nodes[node].data,
-                                                                 '!!!' if node_name == item else '')
-            success = False
+    for node in tree.nodes:
+        if node != '<root>':
+            node_name = tree.nodes[node].tag
+            multi_ver = len(flat.get(node_name, [])) > 1
+            spaces = ((tree.depth() - tree.depth(node)) * 4) + (max_len - len(node_name))
+            tree.nodes[node].tag += '{}    {}    {}'.format(' ' * spaces,
+                                                            tree.nodes[node].data,
+                                                            '!!!' if multi_ver else '')
+            success = success and (not multi_ver)
 
     sys.stderr.write(str(tree))
 
