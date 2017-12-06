2450cb3ba08df4a475f708acba0e097b18627427
Aleksey Burov <allburov@gmail.com>
2017-12-06 15:31:28 +0700

    Public interface testing

--- crosspm/helpers/package.py
+++ crosspm/helpers/package.py
@@ -11,36 +11,35 @@ from crosspm.helpers.archive import Archive
 class Package:
     def __init__(self, name, pkg, params, downloader, adapter, parser, params_found=None, params_found_raw=None,
                  stat=None, in_cache=False):
-        self._packed_path = ''
-        self._unpacked_path = ''
+        self.name = name
+        self.packed_path = ''
+        self.unpacked_path = ''
+        self.duplicated = False
         self.packages = OrderedDict()
+
+        if isinstance(pkg, int):
+            if pkg == 0:
+                self._root = True
+
         self._raw = []
         self._root = False
         self._params_found = {}
         self._params_found_raw = {}
         self._not_cached = True
         self._log = logging.getLogger('crosspm')
-        if isinstance(pkg, int):
-            if pkg == 0:
-                self._root = True
-        self.name = name
-        self.package_name = name  # unique name (* column)
-        self.duplicated = False
+
         self._pkg = pkg
         self._params = params
         self._adapter = adapter
         self._parser = parser
         self._downloader = downloader
         self._in_cache = in_cache
+
         if params_found:
             self._params_found = params_found
         if params_found_raw:
             self._params_found_raw = params_found_raw
         self.stat = stat
-
-    def init_path(self):
-        unp_exists, unp_path = self._downloader.cache.exists_unpacked(package=self, pkg_path=self._unpacked_path)
-        self._unpacked_path = unp_path
 
     def download(self, force=False):
         """
@@ -48,11 +47,11 @@ class Package:
         :param force: Force download even if it seems file already exists
         :return: Full path with filename of downloaded package file.
         """
-        exists, dest_path = self._downloader.cache.exists_packed(package=self, pkg_path=self._packed_path,
+        exists, dest_path = self._downloader.cache.exists_packed(package=self, pkg_path=self.packed_path,
                                                                  check_stat=not self._in_cache)
-        if exists and not self._packed_path:
-            self._packed_path = dest_path
-        unp_exists, unp_path = self._downloader.cache.exists_unpacked(package=self, pkg_path=self._unpacked_path)
+        if exists and not self.packed_path:
+            self.packed_path = dest_path
+        unp_exists, unp_path = self._downloader.cache.exists_unpacked(package=self, pkg_path=self.unpacked_path)
 
         if force or not exists:
             # _packed_path = self._packed_path
@@ -61,28 +60,28 @@ class Package:
                 os.remove(dest_path_tmp)
             self._adapter.download_package(self._pkg, dest_path_tmp)
             os.rename(dest_path_tmp, dest_path)
-            self._packed_path = dest_path
+            self.packed_path = dest_path
             # if not _packed_path:
             self._not_cached = True
         else:
-            if unp_exists and not self._unpacked_path:
-                self._unpacked_path = unp_path
+            if unp_exists and not self.unpacked_path:
+                self.unpacked_path = unp_path
                 self._not_cached = False
 
         if self._not_cached and unp_exists:
             shutil.rmtree(unp_path, ignore_errors=True)
 
-        return self._packed_path
+        return self.packed_path
 
-    def get_file(self, file_name):
-        self.unpack()
+    def get_file(self, file_name, unpack_force=True):
+        if unpack_force:
+            self.unpack()
         _dest_file = self.get_file_path(file_name)
         _dest_file = _dest_file if os.path.isfile(_dest_file) else None
-
         return _dest_file
 
     def get_file_path(self, file_name):
-        _dest_file = os.path.join(self._unpacked_path, file_name)
+        _dest_file = os.path.join(self.unpacked_path, file_name)
         return _dest_file
 
     def find_dependencies(self, depslock_file_path):
@@ -91,11 +90,11 @@ class Package:
 
     def unpack(self, force=False):
         if self._downloader.solid(self):
-            self._unpacked_path = self._packed_path
+            self.unpacked_path = self.packed_path
         else:
-            exists, dest_path = self._downloader.cache.exists_unpacked(package=self, pkg_path=self._unpacked_path)
-            if exists and not self._unpacked_path:
-                self._unpacked_path = dest_path
+            exists, dest_path = self._downloader.cache.exists_unpacked(package=self, pkg_path=self.unpacked_path)
+            if exists and not self.unpacked_path:
+                self.unpacked_path = dest_path
 
             # if force or not exists:
 
@@ -104,14 +103,14 @@ class Package:
             # temp_path = os.path.realpath(os.path.join(dest_path, self._name))
             # _exists = os.path.exists(temp_path)
             if not self._not_cached:
-                self._unpacked_path = dest_path if exists else ''  # temp_path if exists else ''
+                self.unpacked_path = dest_path if exists else ''  # temp_path if exists else ''
             if force or self._not_cached or (not exists):
-                Archive.extract(self._packed_path, dest_path)  # temp_path)
-                self._unpacked_path = dest_path  # temp_path
+                Archive.extract(self.packed_path, dest_path)  # temp_path)
+                self.unpacked_path = dest_path  # temp_path
                 self._not_cached = False
 
     def pack(self, src_path):
-        Archive.create(self._packed_path, src_path)
+        Archive.create(self.packed_path, src_path)
 
     def print(self, level=0, output=None):
         def do_print(left):
@@ -134,9 +133,9 @@ class Package:
         if not self._root:
             if self.duplicated:
                 _sign = '!'
-            elif self._unpacked_path:
+            elif self.unpacked_path:
                 _sign = '+'
-            elif self._packed_path:
+            elif self.packed_path:
                 _sign = '>'
             else:
                 _sign = '-'
@@ -162,7 +161,7 @@ class Package:
             result = {k: v for k, v in self._params_found.items()}
             result.update({k: v for k, v in self._params.items() if k not in result})
         if get_path:
-            result['path'] = self._unpacked_path
+            result['path'] = self.unpacked_path
         if merged:
             result.update(self._parser.merge_valued(result))
         if raw:
@@ -172,6 +171,11 @@ class Package:
     def set_full_unique_name(self):
         self.name = self._parser.get_full_package_name(self)
         return self.name
+
+    def get_name_and_path(self, name_only=False):
+        if name_only:
+            return self.name
+        return self.name, self.unpacked_path
 
     def get_none_packages(self):
         """
--- tests/test_package.py
+++ tests/test_package.py
@@ -1,12 +1,17 @@
 # -*- coding: utf-8 -*-
+import os
 from collections import OrderedDict
+from unittest import TestCase
 
 import pytest
 
 from crosspm.helpers.package import Package
 
 
-class TestPackage():
+class TestPackage(TestCase):
+    _package = Package('package', None, None, None, None, None, None, None, None)
+    _package.unpacked_path = "/test/path"
+
     _root = Package('root', None, None, None, None, None, None, None, None)
     _package1 = Package('package1', None, None, None, None, None, None, None, None)
     _package11 = Package('package11', None, None, None, None, None, None, None, None)
@@ -21,3 +26,22 @@ class TestPackage():
     def test_package_tree(self):
         assert self._root is not None
         assert self._root.all_packages == [self._package11, self._package12, self._package2, self._package1]
+
+    def test_public_interface(self):
+        """
+        Test public interface, which documented in docs/USAGE-PYTHON.md and people can use this attribute in python-code
+        """
+        package = self._package
+        assert hasattr(package, 'name')
+        assert hasattr(package, 'packed_path')
+        assert hasattr(package, 'unpacked_path')
+        assert hasattr(package, 'duplicated')
+
+        assert hasattr(package, 'packages')
+        assert isinstance(package.packages, dict)
+
+        # Legacy get_name_and_path
+        assert package.get_name_and_path() == (package.name, package.unpacked_path)
+
+        assert os.path.normpath(package.get_file_path('some.exe')) == os.path.normpath('/test/path/some.exe')
+        assert package.get_file('not-exist.exe', unpack_force=False) is None
