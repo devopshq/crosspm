ceac0d5cea82f3dd58a2d0e6ba100eaa332b4413
Aleksey Burov <allburov@gmail.com>
2017-12-07 18:17:57 +0700

    stdout, python, json test

--- tests/test_output.py
+++ tests/test_output.py
@@ -1,15 +1,7 @@
-import pytest
+import sys
+from io import StringIO
 
-
-
-
-
-def test_output_type_stdout():
-    assert True
-
-
-def test_output_type_lock():
-    assert True
+from crosspm.helpers.output import LIST, PLAIN, DICT
 
 
 def test_output_type_shell_one(package, output):
@@ -52,5 +44,69 @@ set PACKAGE12_ROOT=/test/path/package12
 set PACKAGE1_ROOT=/test/path/package1
 
 """
-    result = output.output_type_shell(packages=package_root.packages)
+    result = output.output_type_cmd(packages=package_root.packages)
     assert expected_result == result
+
+
+def test_output_type_python_list(package_root, output):
+    expected_result = """# -*- coding: utf-8 -*-
+
+PACKAGES_ROOT = ["""
+    output._output_config['type'] = LIST
+    result = output.output_type_python(packages=package_root.packages)
+    assert result.startswith(expected_result)
+
+
+def test_output_type_python_dict(package_root, output):
+    expected_result = """# -*- coding: utf-8 -*-
+
+PACKAGES_ROOT = {"""
+    output._output_config['type'] = DICT
+    result = output.output_type_python(packages=package_root.packages)
+    assert result.startswith(expected_result)
+
+
+def test_output_type_python_plain(package_root, output):
+    expected_result = """# -*- coding: utf-8 -*-
+
+PACKAGE11_ROOT = '/test/path/package11'
+PACKAGE12_ROOT = '/test/path/package12'
+PACKAGE1_ROOT = '/test/path/package1'
+PACKAGE2_ROOT = '/test/path/package2'
+"""
+    output._output_config['type'] = PLAIN
+    result = output.output_type_python(packages=package_root.packages)
+    assert result == expected_result
+
+
+def test_output_type_json(package_root, output):
+    expected_result = """{
+ "PACKAGES_ROOT": {
+  "PACKAGE2_ROOT": "/test/path/package2",
+  "PACKAGE11_ROOT": "/test/path/package11",
+  "PACKAGE12_ROOT": "/test/path/package12",
+  "PACKAGE1_ROOT": "/test/path/package1"
+ }
+}"""
+    result = output.output_type_json(packages=package_root.packages)
+    assert result == expected_result
+
+
+def test_output_type_stdout(package_root, output):
+    expected_result = """PACKAGE2_ROOT: /test/path/package2
+PACKAGE11_ROOT: /test/path/package11
+PACKAGE12_ROOT: /test/path/package12
+PACKAGE1_ROOT: /test/path/package1
+"""
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = StringIO()
+
+    output.output_type_stdout(packages=package_root.packages)
+
+    sys.stdout = old_stdout
+    mystdout.seek(0)
+    assert expected_result == mystdout.read()
+
+
+def test_output_type_lock():
+    assert True
