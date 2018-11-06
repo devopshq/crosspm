c808846b62d5d1843fc4efbba94fd674805b804e
Aleksey Burov <allburov@gmail.com>
2018-11-06 13:57:21 +0700

    readme as long_desc

--- setup.py
+++ setup.py
@@ -28,47 +28,8 @@ if travis:
     with open('crosspm/config.py', 'w', encoding="utf-8") as f:
         f.write("__version__ = '{}'".format(version))
 
-try:
-    import pypandoc
-
-    print("Converting README...")
-    long_description = pypandoc.convert('README.md', 'rst')
-    if branch:
-        long_description = long_description.replace('crosspm.svg?branch=master', 'crosspm.svg?branch={}'.format(branch))
-    index_begin = long_description.find('\n*Index:*')
-    index_end = long_description.find('\nIntroduction')
-    examples = long_description.find("\nYou'll see something")
-    links = min((long_description.find('\n.. |build'),
-                 long_description.find('\n.. |codacy'),
-                 long_description.find('\n.. |pypi'),
-                 long_description.find('\n.. |license'),
-                 ))
-    if all((index_begin >= 0,
-            index_end >= 0,
-            examples >= 0,
-            links >= 0,
-            )):
-        long_description = '{}{}More information here: {}\n{}'.format(
-            long_description[:index_begin],
-            long_description[index_end:examples + 1],
-            url,
-            long_description[links:].replace('\n', '').replace('.. |', '\n.. |'),
-        )  # .replace('\r\n', '\n')
-
-except (IOError, ImportError, OSError):
-    print("Pandoc not found. Long_description conversion failure.")
-    with open('README.md', encoding="utf-8") as f:
-        long_description = f.read()
-else:
-    print("Saving README.rst...")
-    try:
-        if len(long_description) > 0:
-            with open('README.rst', 'w', encoding="utf-8") as f:
-                f.write(long_description)
-            if travis:
-                os.remove('README.md')
-    except:
-        print("  failed!")
+with open('README.md', encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name='crosspm',
@@ -78,7 +39,7 @@ setup(
     author='Alexander Kovalev',
     author_email='ak@alkov.pro',
     url=url,
-    # long_description=long_description,
+    long_description=long_description,
     download_url='https://github.com/devopshq/crosspm.git',
     entry_points={'console_scripts': ['crosspm=crosspm.__main__:main']},
     classifiers=[
