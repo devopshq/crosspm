75a50f895b3864e07d8e0368332834407a0e7f7a
Aleksey Burov <allburov@gmail.com>
2018-11-08 12:05:54 +0700

    Fix long path bug on Windows OS

--- crosspm/helpers/archive.py
+++ crosspm/helpers/archive.py
@@ -7,6 +7,8 @@ import zipfile
 
 from crosspm.helpers.exceptions import *  # noqa
 
+WINDOWS = True if sys.platform == 'win32' else False
+
 
 class Archive:
     @staticmethod
@@ -42,6 +44,10 @@ class Archive:
 
     @staticmethod
     def extract(archive_name, dst_dir_path, file_name=None):
+        # HACK for https://bugs.python.org/issue18199
+        if WINDOWS:
+            dst_dir_path = "\\\\?\\" + dst_dir_path
+
         dst_dir_path_tmp = '{}_tmp'.format(dst_dir_path)
 
         # remove temp dir
@@ -58,7 +64,6 @@ class Archive:
             # Распаковываем deb в первый раз, сохраняя data.tar рядом с deb-файлом
             # Затем, подменяет имя архива и пускаем по обычному пути распаковки,
             # чтобы он нашел tar и распаковал его как обычно
-            WINDOWS = True if sys.platform == 'win32' else False
             if WINDOWS:
                 datatar_dir = os.path.dirname(archive_name)
                 if not os.path.exists(datatar_dir):
