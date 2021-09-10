d42811978ce9f221e8dc39a5c4bb4db1375183ba
suzinger <suzinger@ptsecurity.com>
2021-09-10 18:56:33 +0500

    CM-39971 Fix Infinite loop when downloads package with lock file

--- crosspm/helpers/package.py
+++ crosspm/helpers/package.py
@@ -97,7 +97,7 @@ class Package:
             self.unpack()
         _dest_file = os.path.normpath(self.get_file_path(file_name))
         _dest_file = _dest_file if os.path.isfile(_dest_file) else None
-        _dest_file = _dest_file if not re.match(".*txt.lock", _dest_file) else None
+        _dest_file = _dest_file if os.path.isfile(_dest_file) and not re.match(".*txt.lock", _dest_file) else None
         return _dest_file
 
     def get_file_path(self, file_name):
