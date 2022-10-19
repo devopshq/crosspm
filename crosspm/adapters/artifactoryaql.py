d712c879b5ab6e25bc6129f3a5f1332d9870f9ea
Anton Zolotukhin <azolotukhin@ptsecurity.ru>
2022-10-19 19:45:51 +0700

    disable recursion lock-file fallback if not found

--- crosspm/adapters/artifactoryaql.py
+++ crosspm/adapters/artifactoryaql.py
@@ -273,8 +273,9 @@ class Adapter(BaseAdapter):
                 if downloader.do_load:
                     _package.download()
                     _deps_file = _package.get_file(self._config.deps_lock_file_name)
-                    if not _deps_file:
-                        _deps_file = _package.get_file(CROSSPM_DEPENDENCY_LOCK_FILENAME)
+                    # TODO: turn on this fallback after testing
+                    # if not _deps_file:
+                    #    _deps_file = _package.get_file(CROSSPM_DEPENDENCY_LOCK_FILENAME)
                     if downloader.recursive:
                         if _deps_file:
                             _package.find_dependencies(_deps_file, property_validate=False)
--- crosspm/adapters/files.py
+++ crosspm/adapters/files.py
@@ -210,8 +210,9 @@ class Adapter(BaseAdapter):
                 if downloader.do_load:
                     _package.download()
                     _deps_file = _package.get_file(self._config.deps_lock_file_name)
-                    if not _deps_file:
-                        _deps_file = _package.get_file(CROSSPM_DEPENDENCY_LOCK_FILENAME)
+                    # TODO: turn on this fallback after testing
+                    # if not _deps_file:
+                    #    _deps_file = _package.get_file(CROSSPM_DEPENDENCY_LOCK_FILENAME)
                     if _deps_file:
                         _package.find_dependencies(_deps_file)
                     elif self._config.deps_file_name:
