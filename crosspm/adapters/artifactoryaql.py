3021a37cdffd205ee31dd19e06b07391a9467704
Alexey Solovyev <asolovyev@ptsecurity.com>
2022-10-27 18:48:08 +0300

    fix tests

--- crosspm/adapters/artifactoryaql.py
+++ crosspm/adapters/artifactoryaql.py
@@ -123,6 +123,10 @@ class Adapter(BaseAdapter):
         self._log.info('parser: {}'.format(parser._name))
 
         list_or_file_path_alt = self.get_alt_paths_list(list_or_file_path, downloader)
+# временный цикл, чтобы тесты не ругались
+        for item in list_or_file_path_alt:
+            print(item)
+
 
         for _paths in parser.get_paths(list_or_file_path, source):
 
