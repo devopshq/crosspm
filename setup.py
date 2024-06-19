5a24fe1b0f8b3920ec9a006b5f0a4b54171691be
vasokot <vasokot@gmail.com>
2024-06-19 09:50:56 +0700

    Upgrade to work at python 3.10+

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     long_description=long_description,
     download_url='https://github.com/devopshq/crosspm.git',
     entry_points={'console_scripts': ['crosspm=crosspm.__main__:main']},
-    python_requires='>=3.6.0',
+    python_requires='>=3.8.0',
     classifiers=[
         'Development Status :: {}'.format(develop_status),
         'Environment :: Console',
@@ -50,9 +50,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     keywords=[
         'development',
@@ -75,19 +76,17 @@ setup(
     tests_require=[
         "pytest>=5.2",
         "pytest-flask>=1.0.0",
-        "PyYAML>=5.2",
+        "PyYAML>=6.0",
     ],
     install_requires=[
-        "requests>=2.25.1,<3.0.0",
-        'urllib3<1.25,>=1.21.1',
+        "requests>=2.30.0,<3.0.0",
+        'urllib3>=2.2.0',
         'docopt==0.6.2',
-        "PyYAML>=5.2,<6.0",
+        "PyYAML>=6.0",
         "dohq-artifactory>=0.8.3",
-        "Jinja2>=2.11",
+        "Jinja2>=3.1.3",
         'patool==1.12',  # need for pyunpack
         'pyunpack==0.2',
-        # 'pyopenssl>=16.2.0',
-        # 'cryptography>=1.7',
     ],
     package_data={
         '': [
